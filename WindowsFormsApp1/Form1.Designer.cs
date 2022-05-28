ec0ed66b4bb20b6ccb2b7e3b09fe841ad330154b
Михаил <@DESKTOP-3P2543V>
2022-05-28 20:46:47 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2bit = new System.Windows.Forms.RadioButton();
+            this.radioButton4bit = new System.Windows.Forms.RadioButton();
+            this.radioButton6bit = new System.Windows.Forms.RadioButton();
+            this.textBoxVvod = new System.Windows.Forms.TextBox();
+            this.textBoxVivod = new System.Windows.Forms.TextBox();
+            this.ChartR = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonx = new System.Windows.Forms.Button();
+            this.buttony = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.colorx = new System.Windows.Forms.ColorDialog();
+            this.colory = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartR)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2bit
+            // 
+            this.radioButton2bit.AutoSize = true;
+            this.radioButton2bit.Checked = true;
+            this.radioButton2bit.Location = new System.Drawing.Point(13, 19);
+            this.radioButton2bit.Name = "radioButton2bit";
+            this.radioButton2bit.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2bit.TabIndex = 1;
+            this.radioButton2bit.TabStop = true;
+            this.radioButton2bit.Text = "2bit";
+            this.radioButton2bit.UseVisualStyleBackColor = true;
+            this.radioButton2bit.Click += new System.EventHandler(this.radioButton2bit_Click);
+            // 
+            // radioButton4bit
+            // 
+            this.radioButton4bit.AutoSize = true;
+            this.radioButton4bit.Location = new System.Drawing.Point(13, 42);
+            this.radioButton4bit.Name = "radioButton4bit";
+            this.radioButton4bit.Size = new System.Drawing.Size(42, 17);
+            this.radioButton4bit.TabIndex = 2;
+            this.radioButton4bit.Text = "4bit";
+            this.radioButton4bit.UseVisualStyleBackColor = true;
+            this.radioButton4bit.Click += new System.EventHandler(this.radioButton4bit_Click);
+            // 
+            // radioButton6bit
+            // 
+            this.radioButton6bit.AutoSize = true;
+            this.radioButton6bit.Location = new System.Drawing.Point(13, 65);
+            this.radioButton6bit.Name = "radioButton6bit";
+            this.radioButton6bit.Size = new System.Drawing.Size(42, 17);
+            this.radioButton6bit.TabIndex = 3;
+            this.radioButton6bit.Text = "6bit";
+            this.radioButton6bit.UseVisualStyleBackColor = true;
+            this.radioButton6bit.Click += new System.EventHandler(this.radioButton6bit_Click);
+            // 
+            // textBoxVvod
+            // 
+            this.textBoxVvod.Location = new System.Drawing.Point(6, 38);
+            this.textBoxVvod.Name = "textBoxVvod";
+            this.textBoxVvod.Size = new System.Drawing.Size(206, 20);
+            this.textBoxVvod.TabIndex = 4;
+            // 
+            // textBoxVivod
+            // 
+            this.textBoxVivod.Location = new System.Drawing.Point(6, 61);
+            this.textBoxVivod.Name = "textBoxVivod";
+            this.textBoxVivod.ReadOnly = true;
+            this.textBoxVivod.Size = new System.Drawing.Size(206, 20);
+            this.textBoxVivod.TabIndex = 5;
+            // 
+            // ChartR
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.ChartR.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.ChartR.Legends.Add(legend3);
+            this.ChartR.Location = new System.Drawing.Point(6, 6);
+            this.ChartR.Name = "ChartR";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.ChartR.Series.Add(series3);
+            this.ChartR.Size = new System.Drawing.Size(1174, 576);
+            this.ChartR.TabIndex = 6;
+            this.ChartR.Text = "chart1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1194, 614);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBoxVvod);
+            this.tabPage1.Controls.Add(this.textBoxVivod);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1186, 588);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonx
+            // 
+            this.buttonx.Location = new System.Drawing.Point(6, 19);
+            this.buttonx.Name = "buttonx";
+            this.buttonx.Size = new System.Drawing.Size(171, 23);
+            this.buttonx.TabIndex = 7;
+            this.buttonx.Text = "Изменить цвет оси x";
+            this.buttonx.UseVisualStyleBackColor = true;
+            this.buttonx.Click += new System.EventHandler(this.buttonx_Click);
+            // 
+            // buttony
+            // 
+            this.buttony.Location = new System.Drawing.Point(6, 48);
+            this.buttony.Name = "buttony";
+            this.buttony.Size = new System.Drawing.Size(171, 23);
+            this.buttony.TabIndex = 6;
+            this.buttony.Text = "Изменить цвет оси y";
+            this.buttony.UseVisualStyleBackColor = true;
+            this.buttony.Click += new System.EventHandler(this.buttony_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ChartR);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1186, 588);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введите 8бит сообщение из едениц и нулей";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(183, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Деления изнутри";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(183, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Деления внутри";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(183, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(134, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "Деления пересекают";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2bit);
+            this.groupBox1.Controls.Add(this.radioButton6bit);
+            this.groupBox1.Controls.Add(this.radioButton4bit);
+            this.groupBox1.Location = new System.Drawing.Point(6, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 159);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "На какое переводить ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.buttonx);
+            this.groupBox2.Controls.Add(this.buttony);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(242, 38);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(328, 152);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Формат Гистограммы";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(183, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(90, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "Делений нет";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 638);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChartR)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2bit;
+        private System.Windows.Forms.RadioButton radioButton4bit;
+        private System.Windows.Forms.RadioButton radioButton6bit;
+        private System.Windows.Forms.TextBox textBoxVvod;
+        private System.Windows.Forms.TextBox textBoxVivod;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartR;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ColorDialog colorx;
+        private System.Windows.Forms.Button buttonx;
+        private System.Windows.Forms.Button buttony;
+        private System.Windows.Forms.ColorDialog colory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int bit;
+        private List<string> load = new List<string>();
+        public int[,] Perenos(string s) {
+            int c = 0;
+            if (s.Length % bit == 0)
+            {
+                int[,] B = new int[s.Length / bit, bit];
+                c = 0;
+                for (int i = 0; i < (s.Length / bit); i++)
+                {
+                    for (int j = 0; j < bit; j++)
+                    {
+                        if (s[c] == 48) B[i, j] = 0;
+                        else B[i, j] = 1;
+                        c++;
+                    }
+                }
+                for (int i = 0; i < s.Length / bit; i++)
+                {
+                    textBoxVivod.Text += " ";
+                    for (int j = 0; j < bit; j++)
+                    {
+                        textBoxVivod.Text += "" + B[i, j];
+                    }
+                }
+                return B;
+            }
+            else
+            {
+                int[,] B = new int[s.Length / bit + 1, bit];
+                c = 0;
+                for (int i = 0; i < (s.Length / bit); i++)
+                {
+                    for (int j = 0; j < bit; j++)
+                    {
+                        if (s[c] == 48) B[i, j] = 0;
+                        else B[i, j] = 1;
+                        c++;
+                    }
+                }
+                int ost = bit - (s.Length % bit);
+                for (int j = 0; j < bit; j++)
+                {
+                    if (ost > j)
+                    {
+                        B[(s.Length / bit), j] = 0;
+                    }
+                    else
+                    {
+                        if (s[c] == 48) { B[(s.Length / bit), j] = 0; }
+                        else { B[(s.Length / bit), j] = 1; }
+                        c++;
+                    }
+                }
+
+                for (int i = 0; i < s.Length / bit + 1; i++)
+                {
+                    textBoxVivod.Text += " ";
+                    for (int j = 0; j < bit; j++)
+                    {
+                        textBoxVivod.Text += "" + B[i, j];
+                    }
+                }
+                return B;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            bit = 2;
+            string save = @"save.log";
+
+            using (StreamReader file1 = new StreamReader(save))
+            {
+                string line;
+                while ((line = file1.ReadLine()) != null)
+                {
+                    line = line.Replace("Color", "");
+                    line = line.Replace("[", "");
+                    line = line.Replace("]", "");
+                    line = line.Replace(" ", "");
+                    load.Add(line);
+                }
+                if (load[0] == "2") { radioButton2bit.Checked = true; bit = 2; }
+                if (load[0] == "4") { radioButton4bit.Checked = true; bit = 4; }
+                if (load[0] == "6") { radioButton6bit.Checked = true; bit = 6; }
+                colorx.Color = Color.FromName(load[1]);
+                colory.Color = Color.FromName(load[2]);
+                if (load[3] == "1") { radioButton1.Checked = true; }
+                if (load[3] == "2") { radioButton2.Checked = true; }
+                if (load[3] == "3") { radioButton3.Checked = true; }
+                if (load[3] == "4") { radioButton4.Checked = true; }
+
+            }
+        }
+
+        private void radioButton2bit_Click(object sender, EventArgs e)
+        {
+            bit = 2;
+        }
+
+        private void radioButton4bit_Click(object sender, EventArgs e)
+        {
+            bit = 4;
+        }
+
+        private void radioButton6bit_Click(object sender, EventArgs e)
+        {
+            bit = 6;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string save = @"save.log";
+                string protocol = @"protocol.log";
+                textBoxVivod.Text = "";
+                string s = textBoxVvod.Text;
+                s = s.Replace(" ", "");
+                for(int i = 0; i < s.Length;i++)
+                {
+                    if (s[i] != 48 && s[i] != 49) { throw new FormatException(""); }
+                }
+                int[,] A = new int[s.Length / 8, 8];
+                int c = 0;
+                for (int i = 0; i < s.Length / 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (s[c] == 48) A[i, j] = 0;
+                        else A[i, j] = 1;
+
+                        c++;
+                    }
+                }
+
+                int[,] B = Perenos(s);
+                using (StreamWriter file1 = new StreamWriter(protocol, true))
+                {
+                    string perevod = "Перевод 8бит кода в " + bit.ToString() + "бит: ";
+                    file1.WriteLine("");
+                    file1.WriteLine(perevod);
+
+                    for (int i = 0; i < A.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < A.GetLength(1); j++)
+                        {
+                            file1.Write(A[i, j].ToString());
+                        }
+                        file1.Write(" ");
+                    }
+                    file1.Write(" ---> ");
+                    for (int i = 0; i < B.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < B.GetLength(1); j++)
+                        {
+                            file1.Write(B[i, j].ToString());
+                        }
+                        file1.Write(" ");
+                    }
+                }
+                int[] C = new int[B.GetLength(0)];
+                int chislo = 0;
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    for (int j = 0; j < bit; j++)
+                    {
+                        chislo += B[i, j] * Convert.ToInt32((Math.Pow(bit, bit - j - 1)));
+                    }
+                    C[i] = chislo;
+                    chislo = 0;
+                }
+                ChartR.Series.Clear();
+                ChartR.Legends.Clear();
+                ChartR.ChartAreas[0].AxisY.Interval = 100;
+                ChartR.ChartAreas[0].AxisX.Interval = 1;
+                ChartR.ChartAreas[0].AxisX.LineColor = colorx.Color;
+                ChartR.ChartAreas[0].AxisY.LineColor = colory.Color;
+                ChartR.ChartAreas[0].AxisX.LineWidth = 1;
+                //ChartR.ChartAreas[0].AxisX.LabelStyle.Format = "{0:0,}K";
+                ChartR.ChartAreas[0].AxisY2.Enabled = AxisEnabled.False;
+                //ChartR.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+                //ChartR.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+                if (radioButton1.Checked)
+                {
+                    ChartR.ChartAreas[0].AxisX.MajorTickMark.TickMarkStyle = TickMarkStyle.OutsideArea;
+                    ChartR.ChartAreas[0].AxisY.MajorTickMark.TickMarkStyle = TickMarkStyle.OutsideArea;
+                }
+                if (radioButton2.Checked)
+                {
+                    ChartR.ChartAreas[0].AxisX.MajorTickMark.TickMarkStyle = TickMarkStyle.InsideArea;
+                    ChartR.ChartAreas[0].AxisY.MajorTickMark.TickMarkStyle = TickMarkStyle.InsideArea;
+                }
+                if (radioButton3.Checked)
+                {
+                    ChartR.ChartAreas[0].AxisX.MajorTickMark.TickMarkStyle = TickMarkStyle.AcrossAxis;
+                    ChartR.ChartAreas[0].AxisY.MajorTickMark.TickMarkStyle = TickMarkStyle.AcrossAxis;
+                }
+                if (radioButton4.Checked)
+                {
+                    ChartR.ChartAreas[0].AxisX.MajorTickMark.TickMarkStyle = TickMarkStyle.None;
+                    ChartR.ChartAreas[0].AxisY.MajorTickMark.TickMarkStyle = TickMarkStyle.None;
+                }
+                TickMark TM = new TickMark();
+                TM.TickMarkStyle = (TickMarkStyle)0;
+                var dataPointSeries = new Series
+                {
+                    Name = "Series 1",
+                    Color = Color.Red,
+                    IsVisibleInLegend = true,
+                    ChartType = SeriesChartType.Column
+                };
+
+                for (int i = 0; i < B.GetLength(0); i++)
+                {
+                    dataPointSeries.Points.AddXY(i, C[i]);
+                }
+
+                ChartR.Series.Add(dataPointSeries);
+                using (StreamWriter file1 = new StreamWriter(save))
+                {
+                    if (radioButton2bit.Checked) { file1.WriteLine("2"); }
+                    if (radioButton4bit.Checked) { file1.WriteLine("4"); }
+                    if (radioButton6bit.Checked) { file1.WriteLine("6"); }
+                    if(colorx.Color != null) { file1.WriteLine(colorx.Color.ToString()); }
+                    if(colory.Color != null) { file1.WriteLine(colory.Color.ToString()); }
+                    if (radioButton1.Checked) { file1.WriteLine("1"); }
+                    if (radioButton2.Checked) { file1.WriteLine("2"); }
+                    if(radioButton3.Checked) { file1.WriteLine("3"); }
+                    if (radioButton4.Checked) { file1.WriteLine("4"); }
+                }
+            }
+            catch (FormatException) 
+            {
+                MessageBox.Show("Неверный ввод данных");
+            }
+        }
+
+        private void buttonx_Click(object sender, EventArgs e)
+        {
+            ColorDialog diag = new ColorDialog();
+            diag.ShowDialog();
+            colorx.Color = diag.Color;
+        }
+
+        private void buttony_Click(object sender, EventArgs e)
+        {
+            ColorDialog diag = new ColorDialog();
+            diag.ShowDialog();
+            colory.Color = diag.Color;
+        }
+    }
+}
